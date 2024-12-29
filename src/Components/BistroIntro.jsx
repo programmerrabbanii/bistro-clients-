ecf452bbf6d25768e72be60d729891bfdb09bd6c
programmerrabbanii <developerrabbanisarker@gmail.com>
2024-12-29 17:02:27 +0600

    welcome to bistro page

--- src/Components/BistroIntro.jsx
+++ src/Components/BistroIntro.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import intro from '../assets/images/home/chef-service.jpg';
 
 const BistroIntro = () => {
     return (
-        <div>
-            
+        <div
+            className="bg-cover bg-center h-screen flex items-center justify-center my-20"
+            style={{
+                backgroundImage: `url(${intro})`, 
+            }}
+        >
+            <div className="bg-white p-10 rounded shadow-lg w-[40%] text-center">
+                <h1 className="text-5xl font-bold uppercase text-gray-800">Welcome to Bistro</h1>
+                <p className="mt-4 text-lg text-gray-600">
+                    Experience the finest dining with a touch of elegance.
+                </p>
+            </div>
         </div>
     );
 };
 
-export default BistroIntro; 
+export default BistroIntro;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import BistroIntro from "../Components/BistroIntro";
 import Hcategory from "../Components/Hcategory";
 
 const Home = () => {
     return (
         <div>
-          <Hcategory></Hcategory>            
+          <Hcategory></Hcategory>    
+          <BistroIntro></BistroIntro>        
         </div>
     );
 };
