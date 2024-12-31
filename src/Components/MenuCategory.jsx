19cb50301afe0d40de38855dc0d33a7159e162ab
programmerrabbanii <developerrabbanisarker@gmail.com>
2024-12-31 09:44:50 +0600

    added the orader the all card

--- src/Components/MenuCategory.jsx
+++ src/Components/MenuCategory.jsx
@@ -1,15 +1,23 @@
+import { Link } from "react-router-dom";
 import Cover from "../Shared.jsx/Cover";
 import Items from "../Shared.jsx/Items";
-const MenuCategory = ({ item,title,img,subHeading }) => {
-    
-    console.log(item);
+const MenuCategory = ({ item, title, img, subHeading }) => {
+  console.log(item);
   return (
     <div>
-       { title && <Cover  img={img} title={title} subHeading={subHeading}></Cover>}
+      {title && <Cover img={img} title={title} subHeading={subHeading}></Cover>}
       <div className="grid md:grid-cols-2 gap-4 w-11/12 mx-auto">
         {item.map((item) => (
           <Items item={item} key={item._id}></Items>
         ))}
+      </div>
+      <div className="text-center py-5">
+        <Link
+          to="/order"
+          className="text-lg font-bold text-gray-800 px-4 py-2 rounded-full border-2 border-yellow-500 bg-yellow-100 hover:bg-yellow-500 hover:text-white transition-all duration-300 shadow-md"
+        >
+          ORDER YOUR FAVOURITE FOOD
+        </Link>
       </div>
     </div>
   );
